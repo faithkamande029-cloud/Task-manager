d0419ca0e6368f3373b3040d4f9e4777b6162bc5
Faith Kamande <faithkamande029@gmail.com>
2026-06-10 14:55:03 +0300

    validation error

--- validation.py
+++ validation.py
@@ -5,19 +5,22 @@ def validate_task_title(title):
     if len(title) == 0:
         raise ValueError("Invalid title")
     return True
-    
-    
+
+
 def validate_task_description(description):
     if len(description) == 0:
-        raise ValueError("Invalid description") 
+        raise ValueError("Invalid description")
 
-    if len(description) > 500:
-        raise ValueError("Description is too long") 
-    return True
     
+    if len(description) > 500:
+        raise ValueError("Description too long")
+
+    return True
+
+
 def validate_due_date(due_date):
     try:
         datetime.strptime(due_date, "%Y-%m-%d")
         return True
     except ValueError:
-        raise ValueError("Invalid date format. use YYYY-MM-DD")
+        raise ValueError("Invalid date format! Use YYYY-MM-DD")
